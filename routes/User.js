dc73682a106b9d3b7cc110fe7f7b9d6cb0e72c80
Prashant <pkg180901@gmail.com>
2023-07-06 16:43:57 +0530

    some error

--- routes/User.js
+++ routes/User.js
@@ -5,7 +5,7 @@ import {
   logout,
   getAdminUsers,
   getAdminStats,
-} from "../controllers/User.js";
+} from "../controllers/user.js";
 import { authorizeAdmin, isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
